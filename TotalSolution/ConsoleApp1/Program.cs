c7d04a6df82c78f7f6333d7a03e6b328fef4c36c
masangwoo <masw23@naver.com>
2021-05-03 11:09:54 +0900

    주석

--- TotalSolution/ConsoleApp1/Program.cs
+++ TotalSolution/ConsoleApp1/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace ConsoleFirstApp
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");//wntjr
         }
     }
 }
