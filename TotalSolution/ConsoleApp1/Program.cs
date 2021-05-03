12cd8819124a20fd5bd965a0d2b748ae01d981a6
masangwoo <masw23@naver.com>
2021-05-03 11:12:50 +0900

    주석2

--- TotalSolution/ConsoleApp1/Program.cs
+++ TotalSolution/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");//wntjr
-            Console.Write("hello");
+            Console.Write("hello");//두번째주석
         }
     }
 }
